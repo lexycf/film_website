f8cd9b374ec3bb5b0dfa05dd5ce5e25c74259a78
lexy <cuif@haibaobaoxian.cn>
2024-03-04 18:51:12 +0800

    no message

---
+++ dist/assets/commonStore-0ea4b9b3.js
@@ -0,0 +1,1 @@
+import{d as a}from"./pinia-903e051d.js";const s=a("common",{state:()=>({aa:1,playerInfo:{}}),getters:{changeAA:a=>a.aa++},actions:{Jia(){this.aa++},set_playerInfo(a){this.playerInfo=a}},persist:{key:"common",storage:sessionStorage,paths:["playerInfo"]}});export{s as u};
